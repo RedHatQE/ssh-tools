6e6c8a8e0f9e308f2ca31fa618bcd7b8789d2229
John Sefler <jsefler@redhat.com>
2012-07-11 14:56:59 -0400

    fixed project clj - added java fields

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject com.redhat.qe/ssh-tools "1.0.0-SNAPSHOT"
+(defproject com.redhat.qe/ssh-tools "1.0.1-SNAPSHOT"
   :description "Extensions of trilead ssh and some CLI tools"
+  :java-source-path "src"
+  :javac-options {:debug "on"}
   :dependencies [[com.trilead/trilead-ssh2 "build213-svnkit-1.3-patch"] 
                  [com.redhat.qe/jul.test.records "1.0.1"],
                  [com.redhat.qe/assertions "1.0.2"]])
