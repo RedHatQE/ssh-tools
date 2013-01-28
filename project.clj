870ef696313e9ed7b38a1421d8ca0fe2e1d5d198
John Sefler <jsefler@redhat.com>
2013-01-28 13:38:34 -0500

    updated project.clj for lein version 2

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject com.redhat.qe/ssh-tools "1.0.1-SNAPSHOT"
   :description "Extensions of trilead ssh and some CLI tools"
-  :java-source-path "src"
+  :java-source-path "src" ;lein1
+  :java-source-paths ["src"] ;lein2
   :javac-options {:debug "on"}
   :dependencies [[com.trilead/trilead-ssh2 "build213-svnkit-1.3-patch"] 
                  [com.redhat.qe/jul.test.records "1.0.1"],
