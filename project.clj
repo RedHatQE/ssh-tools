6fc9d521d9f205cd52cc064742af7913e5a95c2b
James Molet <jmolet@redhat.com>
2017-07-05 13:40:07 -0400

    updating trilead dep

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :java-source-paths ["src"] ;lein2
   :javac-options {:debug "on"}
   :dependencies [;[com.trilead/trilead-ssh2 "build213-svnkit-1.3-patch"]
-                 [org.jenkins-ci/trilead-ssh2 "build217-jenkins-9"]
+                 [org.jenkins-ci/trilead-ssh2 "build217-jenkins-11"]
                  [com.redhat.qe/jul.test.records "1.0.1"],
                  [com.redhat.qe/assertions "1.0.2"]]
   :repositories [["jenkins-ci" "https://repo.jenkins-ci.org/public"]])
