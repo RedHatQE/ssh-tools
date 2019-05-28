f0f63268e360559dd137321d81d454a056078ef0
James Molet <jmolet@redhat.com>
2019-05-28 11:32:02 -0400

    bumping ssh and crypto versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.redhat.qe/ssh-tools "2.0.1-SNAPSHOT"
+(defproject com.redhat.qe/ssh-tools "2.0.2-SNAPSHOT"
   :description "A wrapper for sshj and some CLI tools"
   :java-source-paths ["src/main/java"] ;lein2
   :source-paths ["src/main/clojure"]
@@ -6,12 +6,12 @@
   :javac-options {:debug "on"}
   :dependencies [[com.redhat.qe/jul.test.records "1.0.1"],
                  [com.redhat.qe/assertions "1.0.2"]
-                 [com.hierynomus/sshj "0.23.0"]
+                 [com.hierynomus/sshj "0.27.0"]
                  [org.bouncycastle/bcprov-jdk15on "1.57"]
                  [org.bouncycastle/bcpkix-jdk15on "1.57"]
                  [com.jcraft/jzlib "1.1.3"]
                  [org.clojure/clojure "1.8.0"]
-                 [net.i2p.crypto/eddsa "0.2.0"]]
+                 [net.i2p.crypto/eddsa "0.3.0"]]
   :repositories [["jenkins-ci" "https://repo.jenkins-ci.org/public"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
