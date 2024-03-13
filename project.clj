e2ccfe16ce212dfdcc9c0b70a6605755aa434477
Craig Donnelly <crag@redhat.com>
2024-03-13 16:53:28 -0400

    Add LICENSE + fix project defs

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject com.github.redhatqe/ssh-tools "2.1.2-SNAPSHOT"
   :description "A wrapper for sshj and some CLI tools"
   :url "https://github.com/RedHatQE/ssh-tools"
+  :license {:name "GPL-3.0"
+            :comment "GNU General Public License v3.0"
+            :url "https://choosealicense.com/licenses/gpl-3.0"
+            :year 2024
+            :key "gpl-3.0"}
   :java-source-paths ["src/main/java"] ;lein2
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
@@ -12,7 +17,8 @@
                  [com.jcraft/jzlib "1.1.3"]
                  [org.clojure/clojure "1.8.0"]
                  [net.i2p.crypto/eddsa "0.3.0"]]
-  :repositories [["jenkins-ci" "https://repo.jenkins-ci.org/public"]]
+  :repositories [["jenkins-ci" "https://repo.jenkins-ci.org/public"]
+                 ["releases" {:url "https://repo.clojars.org" :creds :gpg}]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]]
